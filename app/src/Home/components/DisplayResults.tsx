e4372c4df91e0756bdbd62a19bc403bbd4846eda
whispyy <jfdurk@gmx.fr>
2021-05-09 00:29:43 -0400

    feat(size): create auto size conversion utils

--- app/src/Home/components/DisplayResults.tsx
+++ app/src/Home/components/DisplayResults.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import autoConvertFromByte from '../../lib/utils/unit-convert';
 import { NodeFolder } from '../../models/node';
 // import './Home.css';
 
@@ -9,9 +10,19 @@ function DisplayResults({ node }: Props) {
 
   return (
     <div>
-      <span>{node.fileCount}</span> | 
-      <span>{node.folderCount}</span> | 
-      <span>{node.size}</span>
+      <span>Total files {node.fileCount}</span> | 
+      <span>Total folders {node.folderCount}</span> | 
+      <span>Size {autoConvertFromByte(node.size)}</span>
+
+      <div>
+        {node && node.subNode.map(el => (
+          <div>
+            {el.isDirectory && <span>dir</span>}
+            {el.isFile && <span>file</span>}
+            <span>{autoConvertFromByte(el.size)}</span> - <span>{el.name}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ app/src/lib/utils/unit-convert.ts
@@ -0,0 +1,35 @@
+const MAX_SIZE: number = 16;
+
+type Unit = { suffix: string, factor: number };
+type Units = { [key: string]: Unit; };
+
+const units: Units = {
+  1: { suffix: 'B', factor: 0 },
+  2: { suffix: 'B', factor: 0 },
+  3: { suffix: 'B', factor: 0 },
+  4: { suffix: 'KB', factor: 1 },
+  5: { suffix: 'KB', factor: 1 },
+  6: { suffix: 'KB', factor: 1 },
+  7: { suffix: 'MB', factor: 2 },
+  8: { suffix: 'MB', factor: 2 },
+  9: { suffix: 'MB', factor: 2 },
+  10: { suffix: 'GB', factor: 3 },
+  11: { suffix: 'GB', factor: 3 },
+  12: { suffix: 'GB', factor: 3 },
+  13: { suffix: 'TB', factor: 4 },
+  14: { suffix: 'TB', factor: 4 },
+  15: { suffix: 'TB', factor: 4 },
+  16: { suffix: 'PB', factor: 5 },
+}
+
+
+function autoConvertFromByte(value: number): string {
+  const size: number = value.toString().length
+  const conversionSize: string = size > MAX_SIZE ? MAX_SIZE.toString() : size.toString();
+  const unit: Unit = units[conversionSize];
+  const convertedValue = Math.round(value / 1000 ** unit.factor);
+
+  return `${convertedValue} ${unit.suffix}`;
+}
+
+export default autoConvertFromByte;
